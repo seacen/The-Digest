6fcf804ca767047abe81156be0e3ecd13bd62b69
Xichang(Seacen) Zhao <xichangzhao@outlook.com>
2015-10-26 04:11:24 +1100

    tag complete

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,21 +1,18 @@
 module SessionsHelper
+  # Log a user in after authenticating, store in session
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+  # Log out for a user
+  def log_out
+    session[:user_id] = nil
+  end
 
-	# Log a user in after authenticating, store in session
-	def log_in user
-		session[:user_id] = user.id
-	end
+  # Return the currently logged in user for this session
+  def curr_user
+    return if @curr_user
 
-	# Return the currently logged in user for this session
-	def curr_user
-		if !@curr_user
-			@curr_user =  User.find_by(id: session[:user_id])
-		end
-		@curr_user
-	end
-
-	# Log out for a user
-	def log_out
-		session[:user_id] = nil
-	end
-
+    @curr_user = User.find_by(id: session[:user_id])
+    @curr_user
+  end
 end
